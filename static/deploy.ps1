96591ab74353b67b325d3c6d450c7b9d3c4c2fa6
seriouslag <Landon.Gavin@gmail.com>
2019-05-13 19:06:08 -0400

    updated pipelines and scripts

--- static/deploy.ps1
+++ static/deploy.ps1
@@ -3,14 +3,6 @@ param(
     [string]$firebaseProject
 )
 
-$dir = Split-Path $MyInvocation.MyCommand.Path
-write-host "dir: $dir";
-
-$a12="$firebaseToken";
-$a13="$firebaseProject";
-
-write-host "vars $($a12 -split '') $($a13 -split '')";
-
 write-host "Installing firebase tools...";
 npm i -g firebase-tools
 write-host "starting deploy...";
