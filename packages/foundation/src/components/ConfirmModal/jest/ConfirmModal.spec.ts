6fc9cfa9f5a5145a4974f39c4edd85519b4396e4
seriouslag <landon.gavin@gmail.com>
2019-05-13 20:44:12 -0400

    added slot expect

---
+++ packages/foundation/src/components/ConfirmModal/jest/ConfirmModal.spec.ts
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils';
+import ConfirmModal from '../ConfirmModal.vue';
+
+const textToTest = 'text';
+
+describe('ConfirmModal', () => {
+  it('renders and contents are shown', () => {
+    const wrapper = shallowMount(ConfirmModal, {
+        propsData: {
+            value: true,
+        },
+        slots: {
+            default: textToTest,
+        },
+    });
+    // TODO add additional expects for other slots
+    expect(wrapper.text()).toContain(textToTest);
+  });
+});
