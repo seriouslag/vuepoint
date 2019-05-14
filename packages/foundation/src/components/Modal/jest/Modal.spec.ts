579a06768ecfa19a60dd8f53902feee33bcc0144
seriouslag <landon.gavin@gmail.com>
2019-05-13 21:09:26 -0400

    added test

---
+++ packages/foundation/src/components/Modal/jest/Modal.spec.ts
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils';
+import Modal from '../Modal.vue';
+
+const textToTest = 'text';
+
+describe('Modal', () => {
+  it('renders and contents are shown', () => {
+    const wrapper = shallowMount(Modal, {
+        propsData: {
+            value: true,
+        },
+        slots: {
+            default: textToTest,
+        },
+    });
+    // TODO add additional expects for other slots
+    expect(wrapper.text()).toContain(textToTest);
+  });
+});
