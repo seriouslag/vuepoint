8081de8f7835463db57b1c915040531e4999bb28
seriouslag <Landon.Gavin@gmail.com>
2019-12-24 00:31:35 -0500

    updates

--- packages/base/foundation/src/filters/Currency/jest/currency.spec.ts
+++ packages/base/foundation/src/filters/Currency/jest/currency.spec.ts
@@ -2,7 +2,7 @@ import currencyFilter from '../currency';
 import values from '../values';
 
 describe('currencyFilter', () => {
-  values.forEach((item, index: number) => {
+  values.forEach((item) => {
     it(`${item[0]} to be formated to ${item[1]}`, () => {
       expect(currencyFilter(item[0])).toEqual(item[1]);
     });
---
+++ packages/base/foundation/vue.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const cwd = process.cwd();
+const pkg = require(path.join(cwd, 'package.json'));
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const peerDeps = pkg.peerDependencies ? Object.keys(pkg.peerDependencies).reduce((list, dep) => {
+  list[dep] = dep;
+  return list;
+}, {}) : {};
+
+module.exports = {
+  // Can fix issues with declaration files
+  parallel: false,
+  css: {
+    extract: true,
+    sourceMap: !isProd,
+  },
+  configureWebpack: (config) => {
+    config.externals = {
+      ...config.externals,
+      ...peerDeps,
+    };
+
+    config.module.rules.forEach((rule) => {
+      if (rule.use) {
+        const idx = rule.use.findIndex((w) => w.loader === 'thread-loader');
+        if (idx !== -1) {
+          rule.use.splice(idx, 1);
+        }
+      }
+    });
+  },
+  chainWebpack: (config) => {
+    // fix vue issue not generateing declartation files
+    config.module.rule('ts').uses.delete('cache-loader');
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .loader('ts-loader')
+      .tap((options) => {
+        options.transpileOnly = false;
+        options.happyPackMode = false;
+        return options;
+      });
+  },
+};
